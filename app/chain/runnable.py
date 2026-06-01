20b8601e3b9fc7b6c50280ca213ea06fa0e10e01
Piexgear <davidpuscas@live.se>
2026-06-01 14:09:29 +0200

    moved the runnable for the strutcture and made name changes

---
+++ app/chain/runnable.py
@@ -0,0 +1,54 @@
+from typing import Any, Callable, Generic, TypeVar
+
+from pydantic import BaseModel, ConfigDict
+
+I = TypeVar("I")
+O = TypeVar("O")
+M = TypeVar("M")
+
+class Runnable(BaseModel, Generic[I, O]):
+    model_config = ConfigDict(arbitrary_types_allowed = True)
+
+    name: str |  None = None
+
+    def invoke(self, data: I) -> O:
+        raise NotImplementedError("Subclasses is not implemented")
+    
+    def __or__(self, other: Any) -> 'RunnableSequence':
+        if isinstance(other, Runnable):
+            return runnableseSequence.model_construct(firts = self, second = other)
+        if callable(other):
+            return RunnableSequence.model_construct(
+                first = self,
+                second = RunnableLambda.model_construct(func = other, name = other.__name__),
+                name = other.__name__,
+            )
+        return NotImplemented
+    
+    def __ror__(self, other: Any) -> Any:
+        if callable(other):
+            return RunnableSequence.model_construct(
+                first = RunnableLambda.model_construct(func = other),
+                second = self,
+                name = other.__name__,
+            )
+        return NotImplemented
+    
+class RunnableLambda(Runnable[I, O]):
+    func: Callable[[I], O]
+
+    def invoke(self, data: I) -> O:
+        return self.func(data)
+    
+class RunnableSequence(Runnable[I, O], Generic[I, M, O]):
+    func: callable[[I], M]
+
+    def invoke(self, data: I) -> O:
+        return self.func(data)
+    
+class RunnableSequence(Runnable[I, O], Generic[I, M, O]):
+    first: Any 
+    second: Any
+    
+    def invoke(self, data: I) -> O:
+        return self.second.invoke(self.first.invoke(data))
