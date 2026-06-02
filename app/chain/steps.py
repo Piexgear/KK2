511b21aa925b2d318f51b16c036a345d78dd9a81
Piexgear <davidpuscas@live.se>
2026-06-02 09:59:32 +0200

    Added the class components of llmrunner and response parse.

--- app/chain/steps.py
+++ app/chain/steps.py
@@ -2,6 +2,8 @@ from transformers import pipeline
 from app.schemas import PromptBuilderInput, PromptBuilderOutput, LLMRunnerOutput, ResponseParserOutput
 from runnable import Runnable
 
+generator = pipeline("text-generation", model="HuggingFaceTB/SmolLM2-135M-Instruct")
+
 class PromptBuilder(
     Runnable[PromptBuilderInput, PromptBuilderOutput]
 ):
@@ -17,3 +19,17 @@ class PromptBuilder(
         Svara kort och koncist.
         """
         return PromptBuilderOutput(prompt=prompt)
+    
+class LLMRunner(
+    Runnable[PromptBuilderOutput, LLMRunnerOutput]
+):
+    def invoke(self, data: PromptBuilderOutput) -> LLMRunnerOutput:
+        result = generator(data.prompt, max_new_tokens = 100, do_sample = False)
+
+        return LLMRunnerOutput(response=result[0]['generated_text'], model="HuggingFaceTB/SmolLM2-135M-Instruct")
+    
+class ResponseParser(
+    Runnable[LLMRunnerOutput, ResponseParserOutput]
+):
+    def invoke(self, data: LLMRunnerOutput) -> ResponseParserOutput:
+        return ResponseParserOutput(answer=data.response)
