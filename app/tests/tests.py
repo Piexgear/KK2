72d03f209054d4d9633347abab7d441c118b218a
Piexgear <davidpuscas@live.se>
2026-06-04 11:14:02 +0200

    valid and invalid upload tests
    these tests are to ensure that you can only upload csv files and in the
    valid file i also check if the metadata is acurate

--- app/tests/tests.py
+++ app/tests/tests.py
@@ -8,3 +8,21 @@ def test_health():
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
+
+
+def test_upload_invalid_file():
+    
+    response = client.post("/data/upload", files={"file": ("csv.txt", "hej", "text/plain")})
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Only CSV files allowed"}
+
+def test_upload_valid_file():
+    
+    csv = "Name,Price\nElden ring,10\nTetris,20\n"
+
+    response = client.post("/data/upload", files={"file": ("test.csv", csv, "text/csv")})
+
+    assert response.status_code == 200
+    data = response.json()
+    assert data["rows"] == 2
+    assert data["columns"] == ["Name", "Price"]
